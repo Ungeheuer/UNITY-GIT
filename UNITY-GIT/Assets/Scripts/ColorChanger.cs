b60a66dc4f70d7e1df5b0c39d64cf684f3583cec
Isabel Paehr <isabel.paehr@gmail.com>
2016-04-28 18:01:49 +0200

    scene Isi with teststuff added

---
+++ UNITY-GIT/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChanger : MonoBehaviour {
+	
+	public Color colorStart = Color.red;
+	public Color colorEnd = Color.green;
+	public float duration = 0.5F;
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		rend.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+	}
+}
